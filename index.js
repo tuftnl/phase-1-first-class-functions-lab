97ebbaa9c4b56564528396e9fd5a06dd6284f68a
Aaron Lin <aaron.l.lin.general@gmail.com>
2024-04-02 18:13:04 -0400

    1

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (x) => {
+    return x.slice(0,2)
+}
+const returnLastTwoDrivers = (x) => {
+    return x.slice(-2)
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(x) {
+    return function multiply(fare) {
+        return x * fare
+    }
+}
+const fareDoubler = (x) => {
+    return createFareMultiplier(2)(x)
+}
+const fareTripler = (x) => {
+    return createFareMultiplier(3)(x)
+}
+function selectDifferentDrivers(list, func) {
+    return func(list)
+}
+
